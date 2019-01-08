4826ca2c508e87154f861bbba63ee09430c42c2d
Nwudo Ebuka <Ebuka@Uchennas-MacBook-Pro.local>
2019-01-08 16:19:09 +0100

    third

--- app/src/main/java/com/agonaika/agonaika/Loginemail.java
+++ app/src/main/java/com/agonaika/agonaika/Loginemail.java
@@ -1,22 +1,38 @@
 package com.agonaika.agonaika;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Loginemail extends AppCompatActivity {
 TextView firsttime;
+Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginemail);
         setTitle("Login");
         firsttime = findViewById(R.id.textViewfirsttime);
+        login = findViewById(R.id.emaillogintbn);
 
-       // firsttime.setPaintFlags(firsttime.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+      firsttime.setPaintFlags(firsttime.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(Loginemail.this, Facercognition.class));
+
+
+            }
+
+        });
 
     }
 
