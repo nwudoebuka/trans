df8d38928aa08ccfd51ed1be77effd681ce3d0ba
Nwudo Ebuka <Ebuka@Uchennas-MacBook-Pro.local>
2019-01-15 12:54:43 +0100

    pop ups done

---
+++ app/src/main/java/com/agonaika/agonaika/Timesheet.java
@@ -0,0 +1,13 @@
+package com.agonaika.agonaika;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Timesheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timesheet);
+    }
+}
