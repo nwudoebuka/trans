095dafcf87413cf1e43f68c572b2988ca50d20e8
unknown <nwudoebuka@gmail.com>
2019-01-28 11:39:07 +0100

    finished registeration

--- app/src/main/java/com/agonaika/agonaika/Register2.java
+++ app/src/main/java/com/agonaika/agonaika/Register2.java
@@ -1,7 +1,10 @@
 package com.agonaika.agonaika;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register2 extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Register2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+
+        Button next = findViewById(R.id.reg_next_two);
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(Register2.this, Register3.class));
+
+
+            }
+
+        });
     }
 }
